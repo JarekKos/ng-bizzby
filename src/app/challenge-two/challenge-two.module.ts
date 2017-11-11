2d1e5e1d07567891073fe4a0b87cee3f71d8592a
j.kosterkiewicz <j.kosterkiewicz@gmail.com>
2017-11-11 18:36:56 +0100

    Adding AngularGoogleMapCoreModule to ChallengeTwoModule

--- src/app/challenge-two/challenge-two.module.ts
+++ src/app/challenge-two/challenge-two.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { AgmCoreModule } from '@agm/core';
+
 import { MaterialModule } from '../material/material.module';
 
 import { ChallengeTwoComponent } from './components/challenge-two/challenge-two.component';
@@ -9,6 +11,9 @@ import { GMapComponent } from './components/g-map/g-map.component';
 
 @NgModule({
   imports: [
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyChg8OYibqpPLzVyUpyDIQm9byG6Wx2hNY'
+    }),
     CommonModule,
     MaterialModule,
     FormsModule,
