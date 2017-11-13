8965ac87cff914f23f815ad3e8679c44ddf96f0f
jaroslaw.kosterkiewicz <j.kosterkiewicz@gmail.com>
2017-11-13 20:38:22 +0100

    Removing post coe validation from ChallengeTwoComponent

--- src/app/challenge-two/components/challenge-two/challenge-two.component.ts
+++ src/app/challenge-two/components/challenge-two/challenge-two.component.ts
@@ -20,17 +20,4 @@ export class ChallengeTwoComponent implements OnInit {
   onRangeChange(ev) {
     this.range = ev.value;
   }
-
-  isPostCodeValid() {
-    if (!this.postCode) {
-      return false;
-    }
-
-    if (!/^[A-Z]{1,2}([0-9]{1,2}|[0-9][A-Z])\s*[0-9][A-Z]{2}$/.test(this.postCode)) {
-      return false;
-    }
-
-    return true;
-  }
-
 }
